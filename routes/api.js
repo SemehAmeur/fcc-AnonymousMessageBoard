38a5b50508947fecfecd1cfda4eaaae776dfcdc3
Glitch (handsome-iced-megaraptor) <none>
2025-03-07 02:17:56 +0000

    🐏📠 Checkpoint ./routes/api.js:75033497/842

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,31 @@ module.exports = function (app) {
   
   mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
   
-  let 
+  let replySchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    delete_password: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    reported: {type: Boolean, required: true}
+  })
   
-  app.route('/api/threads/:board');
+  let threadSchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    delete_password: {type: String, required: true},
+    board: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    bumped_on: {type: Date, required: true},
+    reported: {type: Boolean, required: true},
+    replies: [replySchema]
+  })
+  
+  let Reply = mongoose.model("Reply", replySchema);
+  let Thread = mongoose.model("Thread", threadSchema);
+  
+  //app.route('/api/threads/:board');
     
-  app.route('/api/replies/:board');
-
+  //app.route('/api/replies/:board');
+  app.post("/api/threads/:board", (req, res)=>{
+    console.log(req.body)
+    return
+  })
 };
