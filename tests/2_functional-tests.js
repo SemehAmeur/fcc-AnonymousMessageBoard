2f36ffc60479d24518da1b316f93f1241f679022
Glitch (handsome-iced-megaraptor) <none>
2025-03-07 15:03:02 +0000

    🌯🏏 Checkpoint ./tests/2_functional-tests.js:75033497/505

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,20 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  let testThread_id;
+  let testReply_id;
+  let testPass = "testpass";
+  test("Creating a new thread", done =>{
+    chai.request(server).post("/api/threads/test").send({
+      board: 'test555',
+      test: "func test thread",
+      delete_password: testPass
+    }).end((err, res)=>{
+      assert.equal(res.status, 200)
+      let createdThread_id = res.redirects[0].split("/")[res.redirects[0].split("/").length - 1]
+      testThread_id = createdThread_id
+      done()
+    })
+  })
+  
 });
